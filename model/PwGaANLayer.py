ee38a7fd1033a4fd6f332fba3db7bb8e7012fe0f
Peter S <petershen815@126.com>
2021-05-09 17:26:06 +0800

    [SpatAttLayer] Implements the Spatial Attention Layer

---
+++ model/PwGaANLayer.py
@@ -0,0 +1,104 @@
+import dgl
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class PwGaANLayer(nn.Module):
+    def __init__(self, in_dim, out_dim, gate=False):
+        super(PwGaANLayer, self).__init__()
+        # Shared Weight W_a for AttentionNet
+        self.Wa = nn.Linear(in_dim, out_dim, bias=False)
+        # AttentionNet outer linear layer
+        self.att_out_fc = nn.Linear(2 * out_dim, 1, bias=False)
+        # Head gate layer
+        self.gate = gate
+        if self.gate:
+            self.gate_fc = nn.Linear(2 * in_dim + out_dim, 1, bias=False)
+            self.Wg = nn.Linear(in_dim, out_dim, bias=False)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        """ Reinitialize learnable parameters. """
+        gain = nn.init.calculate_gain('relu')
+        # gain = nn.init.calculate_gain('leaky_relu', 0.2)  # TODO: gain - leaky_relu with negative_slope=0.2
+        nn.init.xavier_normal_(self.Wa.weight, gain=gain)
+        nn.init.xavier_normal_(self.att_out_fc.weight, gain=gain)
+        if self.gate:
+            # TODO: gain - sigmoid
+            nn.init.xavier_normal_(self.Wg.weight, gain=gain)
+            nn.init.xavier_normal_(self.gate_fc.weight, gain=gain)
+
+    def edge_attention(self, edges):
+        z_comb = torch.cat([edges.data['pre_w'] * edges.src['z'], edges.dst['z']], dim=1)
+        a = self.att_out_fc(z_comb)
+        return {'e': F.leaky_relu(a)}
+
+    def message_func(self, edges):
+        """ Specify messages to be propagated along edges """
+        # The messages will be sent to the mailbox
+        # mailbox['proj_z']: z->x, so we need z's projected features
+        # mailbox['e']: z->x has a e for attention calculation
+        if self.gate:
+            pwFeat = edges.data['pre_w'] * edges.src['v']
+            return {'proj_z': edges.src['proj_z'], 'e': edges.data['e'], 'pre_v_g': pwFeat}
+        else:
+            return {'proj_z': edges.src['proj_z'], 'e': edges.data['e']}
+
+    def reduce_func(self, nodes):
+        """ Specify how messages are processed and propagated to nodes """
+        # Aggregate features to nodes
+        alpha = F.softmax(nodes.mailbox['e'], dim=1)
+        h = torch.sum(alpha * nodes.mailbox['proj_z'], dim=1)
+
+        # head gates
+        if self.gate:
+            pwFeat = nodes.mailbox['pre_v_g']
+            gateProj = self.Wg(pwFeat)
+            maxFeat = torch.max(gateProj, dim=1)[0]
+            meanFeat = torch.mean(pwFeat, dim=1)
+            gComb = torch.cat([nodes.data['v'], maxFeat, meanFeat], dim=1)
+            gFCVal = self.gate_fc(gComb)
+            gVal = F.sigmoid(gFCVal)
+            h = gVal * h
+            test1 = gFCVal.detach().numpy()
+            test2 = gVal.detach().numpy()
+
+        return {'h': h}
+
+    def forward(self, g: dgl.DGLGraph, feat, proj_feat):
+        # Wa: shared attention to features v (or h for multiple GAT layers)
+        g.ndata['v'] = feat
+        g.ndata['proj_z'] = proj_feat
+
+        z = self.Wa(feat)
+        g.ndata['z'] = z
+
+        # AttentionNet
+        g.apply_edges(self.edge_attention)
+        # Message Passing
+        g.update_all(self.message_func, self.reduce_func)
+        return g.ndata['h']
+
+
+class MultiHeadPwGaANLayer(nn.Module):
+    def __init__(self, in_dim, out_dim, num_heads, merge='cat', gate=False):
+        super(MultiHeadPwGaANLayer, self).__init__()
+        self.gate = gate
+        self.heads = nn.ModuleList()
+        for i in range(num_heads):
+            self.heads.append(PwGaANLayer(in_dim, out_dim, self.gate))
+        self.merge = merge
+
+    def forward(self, g: dgl.DGLGraph, feat, proj_feat):
+        with g.local_scope():
+            head_outs = [attn_head(g, feat, proj_feat) for attn_head in self.heads]
+            if self.merge == 'cat':
+                return torch.cat(head_outs, dim=1)
+            else:
+                return torch.mean(torch.stack(head_outs))
+
+
+if __name__ == '__main__':
+    """ Test """
+    pass
---
+++ model/SpatAttLayer.py
@@ -0,0 +1,47 @@
+import numpy as np
+import dgl
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import PwGaANLayer
+
+
+class SpatAttLayer(nn.Module):
+    def __init__(self, feat_dim, hidden_dim, num_heads, gate=False):
+        super(SpatAttLayer, self).__init__()
+        self.gate = gate
+        self.fwdSpatAttLayer = PwGaANLayer.MultiHeadPwGaANLayer(feat_dim, hidden_dim, num_heads, gate=self.gate)
+        self.bwdSpatAttLayer = PwGaANLayer.MultiHeadPwGaANLayer(feat_dim, hidden_dim, num_heads, gate=self.gate)
+        self.geoSpatAttLayer = PwGaANLayer.MultiHeadPwGaANLayer(feat_dim, hidden_dim, num_heads, gate=self.gate)
+        self.proj_fc = nn.Linear(feat_dim, hidden_dim, bias=False)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        """ Reinitialize learnable parameters. """
+        gain = nn.init.calculate_gain('relu')
+        # gain = nn.init.calculate_gain('leaky_relu', 0.2)  # TODO: gain - leaky_relu with negative_slope=0.2
+        nn.init.xavier_normal_(self.proj_fc.weight, gain=gain)
+
+    def forward(self, fg: dgl.DGLGraph, bg: dgl.DGLGraph, gg: dgl.DGLGraph, feat):
+        proj_feat = self.proj_fc(feat)
+        h_fwd = self.fwdSpatAttLayer(fg, feat, proj_feat)
+        h_bwd = self.bwdSpatAttLayer(bg, feat, proj_feat)
+        h_geo = self.geoSpatAttLayer(gg, feat, proj_feat)
+        h = torch.cat([proj_feat, h_fwd, h_bwd, h_geo], dim=1)
+        return h
+
+
+if __name__ == '__main__':
+    """ Test """
+    GVQ = np.load('GVQ.npy', allow_pickle=True).item()
+    G, V, Q = GVQ['G'], GVQ['V'], GVQ['Q']
+    (dfg, dbg,), _ = dgl.load_graphs('FBGraphs.dgl')
+    (dgg,), _ = dgl.load_graphs('GeoGraph.dgl')
+    V = torch.from_numpy(V)
+
+    spatAttLayer = SpatAttLayer(feat_dim=7, hidden_dim=16, num_heads=3, gate=True)
+    print(V, V.shape)
+    out = spatAttLayer(dfg, dbg, dgg, V)
+    print(out, out.shape)
+    test = out.detach().numpy()
+

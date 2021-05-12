3c7c9a73654029445368fd66033af8317222edf9
Peter S <petershen815@126.com>
2021-05-12 11:12:12 +0800

    [MODEL] Rectifies the behavior of pretraining (if pretrain, G will not be predicted but the module remains the same so that later it can be re-trained)

--- model/Gallat.py
+++ model/Gallat.py
@@ -29,7 +29,7 @@ class Gallat(nn.Module):
         self.tempAttLayer = TempAttLayer(query_dim=self.query_dim, embed_dim=self.spat_embed_dim, rec_merge='sum', comb_merge='sum')
 
         # Transferring Attention Layer
-        self.tranAttLayer = TranAttLayer(embed_dim=self.temp_embed_dim, activate_function_method='sigmoid', predict_G=self.predict_G)
+        self.tranAttLayer = TranAttLayer(embed_dim=self.temp_embed_dim, activate_function_method='sigmoid')
 
     def forward(self, record, query):
         # Extract spatial features
@@ -41,12 +41,9 @@ class Gallat(nn.Module):
         temp_embed = self.tempAttLayer(query, spat_embed_dict)
 
         # Transferring features to perform predictions
-        if self.predict_G:
-            demands, Gtp1 = self.tranAttLayer(temp_embed)
-            return demands, Gtp1
-        else:
-            demands = self.tranAttLayer(temp_embed)
-            return demands
+        res = self.tranAttLayer(temp_embed, self.predict_G)
+
+        return res
 
 
 if __name__ == '__main__':
--- model/TranAttLayer.py
+++ model/TranAttLayer.py
@@ -5,10 +5,9 @@ import torch.nn.functional as F
 
 
 class TranAttLayer(nn.Module):
-    def __init__(self, embed_dim, activate_function_method='sigmoid', predict_G=False):
+    def __init__(self, embed_dim, activate_function_method='sigmoid'):
         super(TranAttLayer, self).__init__()
         self.embed_dim = embed_dim
-        self.predict_G = predict_G
 
         # Demand Prediction Linear Layer + Activation
         self.demand_fc = nn.Linear(self.embed_dim, 1, bias=True)
@@ -20,25 +19,24 @@ class TranAttLayer(nn.Module):
             self.activate_function = nn.Sigmoid()
             gain_str = 'sigmoid'
 
-        if self.predict_G:
-            # Shared Weight W_a for AttentionNet
-            self.Wa = nn.Linear(self.embed_dim, self.embed_dim, bias=False)
-            # AttentionNet outer linear layer
-            # split two fc to avoid concat
-            self.att_out_fc_l = nn.Linear(self.embed_dim, 1, bias=False)
-            self.att_out_fc_r = nn.Linear(self.embed_dim, 1, bias=False)
+        # Shared Weight W_a for AttentionNet
+        self.Wa = nn.Linear(self.embed_dim, self.embed_dim, bias=False)
+        # AttentionNet outer linear layer
+        # split two fc to avoid concat
+        self.att_out_fc_l = nn.Linear(self.embed_dim, 1, bias=False)
+        self.att_out_fc_r = nn.Linear(self.embed_dim, 1, bias=False)
 
         self.reset_parameters(nonlinearity='sigmoid')
 
     def reset_parameters(self, nonlinearity):
         gain = nn.init.calculate_gain(nonlinearity)
         nn.init.xavier_normal_(self.demand_fc.weight, gain=gain)
-        if self.predict_G:
-            # Attention
-            gain = nn.init.calculate_gain('leaky_relu', 0.2)
-            nn.init.xavier_normal_(self.Wa.weight, gain=gain)
-            nn.init.xavier_normal_(self.att_out_fc_l.weight, gain=gain)
-            nn.init.xavier_normal_(self.att_out_fc_r.weight, gain=gain)
+
+        # Attention
+        gain = nn.init.calculate_gain('leaky_relu', 0.2)
+        nn.init.xavier_normal_(self.Wa.weight, gain=gain)
+        nn.init.xavier_normal_(self.att_out_fc_l.weight, gain=gain)
+        nn.init.xavier_normal_(self.att_out_fc_r.weight, gain=gain)
 
     def predict_request_graph_attention(self, embed_feat_sample):
         num_nodes = embed_feat_sample.shape[-2]
@@ -62,7 +60,7 @@ class TranAttLayer(nn.Module):
         Gs = Qs * rel_Ds
         return Gs
 
-    def forward(self, embed_feat):
+    def forward(self, embed_feat, predict_G):
         num_nodes = embed_feat.shape[-2]
 
         # Predict demands
@@ -70,9 +68,9 @@ class TranAttLayer(nn.Module):
         demands = self.activate_function(demands)
         demands = demands.reshape(-1, num_nodes)
 
-        if self.predict_G:
+        if predict_G:
             # Predict Request Graph
             req_gs = self.predict_request_graphs(embed_feat, demands)
             return demands, req_gs
         else:
-            return demands
+            return demands, None

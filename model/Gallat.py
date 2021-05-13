f56ba3382decd1c738750877b7835eb99a80b79a
Peter S <petershen815@126.com>
2021-05-14 06:48:20 +0800

    [MODEL] Removes intermediate ReLU for Gallat, use sigmoid to activate Transfer Layer again

--- model/Gallat.py
+++ model/Gallat.py
@@ -23,23 +23,21 @@ class Gallat(nn.Module):
 
         # Spatial Attention Layer
         self.spatAttLayer = SpatAttLayer(feat_dim=self.feat_dim, hidden_dim=self.hidden_dim, num_heads=1, gate=False)
-        self.spatActivation = nn.ReLU()
 
         # Temporal Attention Layer
         self.tempAttLayer = TempAttLayer(query_dim=self.query_dim, embed_dim=self.spat_embed_dim, rec_merge='sum', comb_merge='sum')
-        self.tempActivation = nn.ReLU()
 
         # Transferring Attention Layer
-        self.tranAttLayer = TranAttLayer(embed_dim=self.temp_embed_dim, activate_function_method=None)
+        self.tranAttLayer = TranAttLayer(embed_dim=self.temp_embed_dim, activate_function_method='sigmoid')
 
     def forward(self, record, query, predict_G=False):
         # Extract spatial features
         spat_embed_dict = {}
         for temp_feat in TEMP_FEAT_NAMES:
-            spat_embed_dict[temp_feat] = [self.spatActivation(self.spatAttLayer(fg, bg, gg)) for (fg, bg, gg) in record[temp_feat]]
+            spat_embed_dict[temp_feat] = [self.spatAttLayer(fg, bg, gg) for (fg, bg, gg) in record[temp_feat]]
 
         # Extract temporal features
-        temp_embed = self.tempActivation(self.tempAttLayer(query, spat_embed_dict))
+        temp_embed = self.tempAttLayer(query, spat_embed_dict)
 
         # Transferring features to perform predictions
         res = self.tranAttLayer(temp_embed, predict_G)

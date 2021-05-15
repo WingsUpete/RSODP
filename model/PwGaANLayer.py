[MODEL] Fixes averaging functions

model/PwGaANLayer.py
@@ -103,4 +103,4 @@ class MultiHeadPwGaANLayer(nn.Module):
         if self.merge == 'cat':
             return torch.cat(head_outs, dim=-1)
         else:
-            return torch.mean(sum(head_outs) / len(head_outs))
+            return sum(head_outs) / len(head_outs)

model/ScaledDotProductAttention.py
@@ -45,7 +45,7 @@ class ScaledDotProductAttention(nn.Module):
         if self.merge == 'sum':
             return sum(embed_outputs)
         elif self.merge == 'mean':
-            return torch.mean(sum(embed_outputs) / len(embed_outputs))
+            return sum(embed_outputs) / len(embed_outputs)
         elif self.merge == 'cat':
             return torch.cat(embed_outputs, dim=-1)
         else:   # Default: sum, as Gallat
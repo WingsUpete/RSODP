0965af4e452942e73d02463ec8c7869bb95ecc89
Peter S <petershen815@126.com>
2021-05-21 13:12:43 +0800

    [MODEL] Makes GallatExt fully functional

--- model/GallatExt.py
+++ model/GallatExt.py
@@ -20,14 +20,14 @@ class GallatExt(nn.Module):
         self.query_dim = query_dim
         self.hidden_dim = hidden_dim
 
-        self.spat_embed_dim = 4 * self.hidden_dim    # Embedding dimension after spatial feature extraction
-        self.temp_embed_dim = self.spat_embed_dim                           # Embedding dimension after temporal feature extraction
+        self.spat_embed_dim = (3 * self.num_heads + 1) * self.hidden_dim    # Embedding dimension after spatial feature extraction
+        self.temp_embed_dim = 4 * self.spat_embed_dim                       # Embedding dimension after temporal feature extraction
 
         # Spatial Attention Layer
-        self.spatAttLayer = SpatAttLayer(feat_dim=self.feat_dim, hidden_dim=self.hidden_dim, num_heads=self.num_heads, gate=True, merge='mean')
+        self.spatAttLayer = SpatAttLayer(feat_dim=self.feat_dim, hidden_dim=self.hidden_dim, num_heads=self.num_heads, gate=True, merge='cat')
 
         # Temporal Attention Layer
-        self.tempAttLayer = TempAttLayer(query_dim=self.query_dim, embed_dim=self.spat_embed_dim, rec_merge='mean', comb_merge='mean')
+        self.tempAttLayer = TempAttLayer(query_dim=self.query_dim, embed_dim=self.spat_embed_dim, rec_merge='mean', comb_merge='cat')
 
         # Transferring Attention Layer
         self.tranAttLayer = TranAttLayer(embed_dim=self.temp_embed_dim, activate_function_method=None)

8f509890c0ae4e7d8048d5f18320adad722613b4
Peter S <petershen815@126.com>
2021-05-25 03:06:09 +0800

    [ANALYSIS] Implements loss curve and avg TTpS analysis from log files

--- utils/Utils.py
+++ utils/Utils.py
@@ -3,6 +3,8 @@ Utility functions
 """
 import math
 import torch
+import os
+import matplotlib.pyplot as plt
 
 ZERO_TENSOR = torch.Tensor([0])
 
@@ -75,10 +77,64 @@ def MAPE(y_pred, y_true, threshold=ZERO_TENSOR):
     return torch.mean(torch.abs((y_true_filter - y_pred_filter)/(y_true_filter + 1)))
 
 
+def path2FileNameWithoutExt(path):
+    """
+    get file name without extension from path
+    :param path: file path
+    :return: file name without extension
+    """
+    return os.path.splitext(path)[0]
+
+
+def trainLog2LossCurve(logfn='train.log'):
+    if not os.path.isfile(logfn):
+        print('{} is not a valid file.'.format(logfn))
+        exit(-1)
+
+    x_epoch = []
+    y_loss_train = []
+    train_time_list = []
+
+    print('Analyzing log file: {}'.format(logfn))
+    f = open(logfn, 'r')
+    lines = f.readlines()
+    for line in lines:
+        if not line.startswith('Training Round'):
+            continue
+        items = line.strip().split(sep=' ')
+
+        epoch = int(items[2][:-1])
+        x_epoch.append(epoch)
+
+        loss = float(items[5][:-1])
+        y_loss_train.append(loss)
+
+        train_time = float(items[10][1:])
+        train_time_list.append(train_time)
+
+    # Count average TTpS
+    avgTTpS = sum(train_time_list) / len(train_time_list)
+    print('Average TTpS: %.4f sec' % avgTTpS)
+
+    # Plot training loss curve
+    print('Plotting loss curve.')
+    plt.plot(x_epoch, y_loss_train, c='purple', label='Train Loss', alpha=0.8)
+    plt.title('Epoch - Training Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend(loc='upper right')
+    # plt.show()
+    figpath = '{}.png'.format(path2FileNameWithoutExt(logfn))
+    plt.savefig(figpath)
+    print('Loss curve saved to {}'.format(figpath))
+
+
+    print('All analysis tasks finished.')
+
+
 # Test
 if __name__ == '__main__':
     # print(haversine((40.4944, -74.2655), (40.9196, -73.6957)))  # 67.39581283189828
-    print(haversine(
-        (40.9196, -74.2655),
-        (40.9196, -73.6957))
-    )
+    # trainLog2LossCurve(logfn='../res/Gallat_retrain/20210522_14_44_12.log')
+    # trainLog2LossCurve(logfn='../res/GallatExt_pretrain/low_dimension/best/20210518_15_20_40.log')
+    trainLog2LossCurve(logfn='../res/GallatExt_retrain/low_dimension/averageG_best/20210519_03_48_57.log')

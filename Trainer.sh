3f02e78e8790a7462a547eceb18e4c5c24a9b5b3
WingsUpete <petershen815@126.com>
2021-06-14 01:54:34 +0000

    [BASH] Updates training bash command file

--- Trainer.sh
+++ Trainer.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 ulimit -n 65535
+# Preprocess
+#python DataPackager.py -d ny2016_0101to0331.csv --minLat 40.4944 --maxLat 40.9196 --minLng -74.2655 --maxLng -73.6957 --refGridH 2.5 --refGridW 2.5 -er 1 -od ../data/ -c 20
 # HA
 python HistoricalAverage.py -dr data/ny2016_0101to0331/ -th 2184 -ts 1 -c 20 -bs 32
 # GallatExt
 python Trainer.py -dr data/ny2016_0101to0331/ -th 2184 -ts 1 -c 20 -m train -tt pretrain -net GallatExt -me 200 -bs 32 -re 0.2
-# python Trainer.py -dr data/ny2016_0101to0331/ -th 2184 -ts 1 -c 20 -m train -tt retrain -r model_save/20210528_11_57_39.pth -me 200 -bs 32 -re 0.2
-# python Trainer.py -dr data/ny2016_0101to0331/ -th 2184 -ts 1 -c 20 -m eval -e model_save/20210530_05_30_19.pth -bs 32
+#python Trainer.py -dr data/ny2016_0101to0331/ -th 2184 -ts 1 -c 20 -m train -tt retrain -r model_save/20210528_11_57_39.pth -me 200 -bs 32 -re 0.2
+#python Trainer.py -dr data/ny2016_0101to0331/ -th 2184 -ts 1 -c 20 -m eval -e model_save/20210530_05_30_19.pth -bs 32

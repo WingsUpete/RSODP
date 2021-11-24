dad46439eac2860eed42ac90f4e2a9cc6acae800
Peter S <petershen815@126.com>
2021-11-24 13:40:29 +0800

    [MODEL] Uses relu to filter negative values in GallatExt for final activation function

--- model/GallatExt.py
+++ model/GallatExt.py
@@ -29,8 +29,8 @@ class GallatExt(nn.Module):
         # Temporal Attention Layer
         self.tempAttLayer = TempAttLayer(query_dim=self.query_dim, embed_dim=self.spat_embed_dim, rec_merge='mean', comb_merge='mean')
 
-        # Transferring Attention Layer TODO: activate with ReLU
-        self.tranAttLayer = TranAttLayer(embed_dim=self.temp_embed_dim, activate_function_method=None)
+        # Transferring Attention Layer
+        self.tranAttLayer = TranAttLayer(embed_dim=self.temp_embed_dim, activate_function_method='relu')
 
     def forward(self, record, query, ref_D=None, ref_G=None, predict_G=False, ref_extent=0.2):
         # Extract spatial features

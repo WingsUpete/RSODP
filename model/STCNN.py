0e970e4c4ba21b407009459068348c72a861653c
Peter S <petershen815@126.com>
2021-12-07 18:46:33 +0800

    [MODEL] Creates Short-Term CNN (STCNN) for LSTNet

---
+++ model/STCNN.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+
+
+class STCNN(nn.Module):
+    def __init__(self, p):
+        super(STCNN, self).__init__()
+		# TODO
+
+    def forward(self, record_GD):
+		pass
+		# TODO
+        # return res_D, res_G
